fbfd870bee0186a10292c0ca89984d34b27270e3
marcomuna <marco.muna143@gmail.com>
2024-02-18 15:11:16 +0530

    variable in javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+// variable in javascript$$$$$$$$$4
+
+let a = 49;
+let b = 9;
+const m = 44
+var string = "muna"  /*var is used in globally all time in it use*/
+
+{
+    let a = 48
+    console.log(a)
+}
+console.log(a+b)
+console.log(string)
+console.log(typeof string , typeof a) 
