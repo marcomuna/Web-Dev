5f4f76ebd3dd62d9ae33b52d28477ff64c3a8c8c
marcomuna <marco.muna143@gmail.com>
2024-02-18 15:37:18 +0530

    conditional statemensts in js

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-// variable in javascript$$$$$$$$$4
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$           variable in javascript$$$$$$$$$4
 
-let a = 49;
-let b = 9;
-const m = 44
-var string = "muna"  /*var is used in globally all time in it use*/
+// let a = 49;
+// let b = 9;
+// const m = 44
+// var string = "muna"  /*var is used in globally all time in it use*/
 
-{
-    let a = 48
-    console.log(a)
+// {
+//     let a = 48
+//     console.log(a)
+// }
+// console.log(a+b)
+// console.log(string)
+// console.log(typeof string , typeof a) 
+
+// ###########################################################################        conditional statemensts in js
+let age = 19;
+
+if(age >= 18){
+    console.log("you can apply for credit card")
 }
-console.log(a+b)
-console.log(string)
-console.log(typeof string , typeof a) 
+else{
+    console.log("you cannot apply for credit card")
+
+}
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ elif condition
+// let age = 13
+// let age = 20
+let age2 = 18
+
+if(age2 < 18){
+    console.log("you cannot drive")
+}
+else if(age2 == 18){
+    console.log("now you are a learner")
+}
+else{
+    console.log("you can drive")
+}
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ternary operator condition to use it in one ine
+
+let a = 14
+let b = 9
+c = a >b ?(a+b):(a-b)
+console.log(c)
