3e6c24dd4536ba451687dd35fc46425d85376307
marcomuna <marco.muna143@gmail.com>
2024-02-18 15:59:24 +0530

    for loop

--- index.js
+++ index.js
@@ -14,34 +14,37 @@
 // console.log(typeof string , typeof a) 
 
 // ###########################################################################        conditional statemensts in js
-let age = 19;
+// let age = 19;
 
-if(age >= 18){
-    console.log("you can apply for credit card")
-}
-else{
-    console.log("you cannot apply for credit card")
+// if(age >= 18){
+//     console.log("you can apply for credit card")
+// }
+// else{
+//     console.log("you cannot apply for credit card")
 
-}
+// }
 
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ elif condition
-// let age = 13
-// let age = 20
-let age2 = 18
+// // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ elif condition
+// // let age = 13
+// // let age = 20
+// let age2 = 18
 
-if(age2 < 18){
-    console.log("you cannot drive")
-}
-else if(age2 == 18){
-    console.log("now you are a learner")
-}
-else{
-    console.log("you can drive")
-}
+// if(age2 < 18){
+//     console.log("you cannot drive")
+// }
+// else if(age2 == 18){
+//     console.log("now you are a learner")
+// }
+// else{
+//     console.log("you can drive")
+// }
 
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ternary operator condition to use it in one ine
 
-let a = 14
-let b = 9
-c = a >b ?(a+b):(a-b)
-console.log(c)
+// let a = 14
+// let b = 9
+// c = a >b ?(a+b):(a-b)
+// console.log(c)
+
+
+
---
+++ loops.js
@@ -0,0 +1,44 @@
+
+// for loop ...............i......
+let i = 0
+
+for (let i = 0; i < 10 ; i++) {
+    console.log(i)
+    
+}
+
+// for in loop .........,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+// let object = {
+//     name:"muna",
+//     occupation:"graduate",
+//     hobby:"computer study"
+
+// }
+// for (const key in object) {
+//     const element = object[key];
+//     console.log(key,":",element)
+// }
+
+// for of loop,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+// for (const c of "muna") {
+//     console.log(c)
+    
+// }
+
+// while loop ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// let i = 0
+
+// while (i<10) {
+//     console.log(i);
+//     i++
+// }
+
+// do while loop,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// let i = 10
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i<8);
