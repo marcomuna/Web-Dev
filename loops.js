e9c8b7d1f16bd226f1cf9c6795d1397686a8c582
marcomuna <marco.muna143@gmail.com>
2024-02-18 16:03:38 +0530

    do while loop

--- loops.js
+++ loops.js
@@ -28,17 +28,17 @@
 // }
 
 // while loop ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-let i = 0
+// let i = 0
 
-while (i<10) {
-    console.log(i);
-    i++
-}
+// while (i<10) {
+//     console.log(i);
+//     i++
+// }
 
 // do while loop,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-// let i = 10
+let i = 10
 
-// do {
-//     console.log(i)
-//     i++
-// } while (i<8);
+do {
+    console.log(i)
+    i++
+} while (i<8);
