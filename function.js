01b117af5d4585a15554a2767f78720b7bfd90ca
marcomuna <marco.muna143@gmail.com>
2024-02-18 16:21:10 +0530

    function in js

---
+++ function.js
@@ -0,0 +1,34 @@
+// function in JSON={,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,}
+
+function name (num) {
+    console.log(`my name is ${num}`)
+    console.log(`what is your hubby ${num}`)
+    console.log(`are you doing some task  ${num}`)
+    
+}
+name("monu")
+name("muni")
+
+
+
+
+
+function sum(a,b,c=8) {
+    console.log(a+b+c)
+    console.log(a+b-c)
+    console.log(a*b+c)
+    
+}
+sum(8,6,5)
+console.log("now second programme")
+sum(2,6)
+console.log("now third programme")
+sum(5)
+
+// arrow function,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+let add = (m)=>{
+    console.log(5+6-m)
+    console.log(5+m*4)
+}
+add(2)
