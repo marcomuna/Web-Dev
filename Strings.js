9b9e8fc3ca7ab3f11f514b0a10d8f03a3b8ae479
marcomuna <marco.muna143@gmail.com>
2024-02-19 14:22:31 +0530

    Strings in js

---
+++ Strings.js
@@ -0,0 +1,15 @@
+console.log("i am muna")
+
+let a = "muna"
+let b = "MUNA"
+
+console.log(a[1])
+console.log(a.length)
+console.log(a.toUpperCase())
+console.log(b.toLowerCase())
+console.log(`${a} in lowercase and ${b} in uppercase`)
+console.log(a.concat(" and ",b))
+console.log(a.slice(0,2))
+console.log(a.replace("m", "hii m"))
+console.log(a.startsWith("m"))
+console.log(a.endsWith("na"))
